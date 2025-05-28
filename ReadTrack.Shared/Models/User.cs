a8fb32875f8b07d247b7bbafd9a16864f8cc3a6a
Christopher Spence <christopher.spence@flikshop.com>
2025-05-28 16:25:46 -0500

    fixed nullable type warnings

--- ReadTrack.Shared/Models/User.cs
+++ ReadTrack.Shared/Models/User.cs
@@ -1,17 +1,16 @@
 using System.Collections.Generic;
-using ReadTrack.Shared.Models;
 
 namespace ReadTrack.Shared.Models;
 
 public class User : BaseModel
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string ProfilePicture { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? ProfilePicture { get; set; }
     public bool IsLocked { get; set; }
 
     // FKs
-    public IEnumerable<Book> Books { get; set; }
+    public IEnumerable<Book>? Books { get; set; }
 }
