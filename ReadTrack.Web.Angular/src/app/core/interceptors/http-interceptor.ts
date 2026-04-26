1f7a51dc7577571ff7d3ecdccde9929330fcd545
Christopher Spence <christopher.spence@flikshop.com>
2026-04-25 19:06:56 -0500

    fixed angular project not loading api

--- ReadTrack.Web.Angular/src/app/core/interceptors/http-interceptor.ts
+++ ReadTrack.Web.Angular/src/app/core/interceptors/http-interceptor.ts
@@ -3,6 +3,7 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, of, throwError } from 'rxjs';
 import { UserService } from '../services';
+import { environment } from 'src/environments/environment';
 
 enum HTTP_STATUS {
     SUCCESS = 200,
@@ -13,9 +14,8 @@ enum HTTP_STATUS {
 
 export const readTrackHttpInterceptor: HttpInterceptorFn = (req, next) => {
     const userService = inject(UserService);
-    const baseUrl = import.meta.env['SERVICES__API__HTTP__0']
     req = req.clone({
-        url: `${baseUrl}/${req.url}`
+        url: `${environment.baseUrl}/${req.url}`
     });
 
     const token = userService.getToken();
